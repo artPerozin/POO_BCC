b6e65df95f0b0c7cabbe0e1bfd757afaf8e38f8e
artPerozin <perozin.arthur@gmail.com>
2025-08-27 15:58:08 -0300

    finalmenteeess

--- Semana3/SistemaDePassagensUML/src/usecases/Main.java
+++ Semana3/SistemaDePassagensUML/src/usecases/Main.java
@@ -59,9 +59,14 @@ public class Main {
     public static void cadastrarCidade() {
         System.out.print("Nome da cidade: ");
         String nomeCidade = sc.nextLine();
+        System.out.print("Estado da cidade: ");
+        String estado = sc.nextLine();
 
         Cidade cidade = new Cidade();
+
         cidade.setNome(nomeCidade);
+        cidade.setEstado(estado);
+
         sistema.cadastrarCidade(cidade);
     }
 
@@ -73,42 +78,101 @@ public class Main {
         }
         System.out.println("Escolha o cliente:");
         for (int i = 0; i < clientes.length; i++) {
-            System.out.println((i + 1) + " \n " + clientes[i]);
+            System.out.println("[" + i + "] " + clientes[i]);
         }
         int escolhaCliente = sc.nextInt();
         sc.nextLine();
-        Cliente cliente = clientes[escolhaCliente - 1];
 
+        Cliente cliente = clientes[escolhaCliente];
         Cidade[] cidades = sistema.mostrarCidades();
+
         if (cidades.length < 2) {
             System.out.println("É necessário ter pelo menos 2 cidades cadastradas!");
             return;
         }
+
         System.out.println("Escolha a cidade de origem:");
         for (int i = 0; i < cidades.length; i++) {
-            System.out.println((i + 1) + " - " + cidades[i]);
+            System.out.println("[" + i + "] " + cidades[i]);
         }
         int origemIndex = sc.nextInt();
         sc.nextLine();
 
         System.out.println("Escolha a cidade de destino:");
         for (int i = 0; i < cidades.length; i++) {
-            if (i == origemIndex - 1) continue;
-            System.out.println((i + 1) + " - " + cidades[i]);
+            if (i == origemIndex) continue;
+            System.out.println("[" + i + "] " + cidades[i]);
         }
         int destinoIndex = sc.nextInt();
         sc.nextLine();
 
-        Cidade origem = cidades[origemIndex - 1];
-        Cidade destino = cidades[destinoIndex - 1];
+        Cidade origem = cidades[origemIndex];
+        Cidade destino = cidades[destinoIndex];
+
+        System.out.print("Número da reserva: ");
+        int numReserva = sc.nextInt();
+        sc.nextLine();
 
         System.out.print("Data da viagem (dd/mm/aaaa): ");
-        String data = sc.nextLine();
+        String dataVoo = sc.nextLine();
+
+        System.out.print("Hora da viagem (hh:mm): ");
+        String horaVoo = sc.nextLine();
+
+        System.out.print("Preço da passagem: R$ ");
+        float preco = sc.nextFloat();
+        sc.nextLine();
+
+        System.out.print("Classe (Econômica, Executiva, Primeira): ");
+        String classeVoo = sc.nextLine();
+
+        System.out.print("Ida e volta? (true/false): ");
+        boolean idaEvolta = sc.nextBoolean();
+        sc.nextLine();
+
+        System.out.print("Número da poltrona: ");
+        int poltrona = sc.nextInt();
+        sc.nextLine();
 
         Reserva reserva = new Reserva();
+        reserva.setNumReserva(numReserva);
+        reserva.setDataVoo(dataVoo);
+        reserva.setHoraVoo(horaVoo);
+        reserva.setPreco(preco);
+        reserva.setClasseVoo(classeVoo);
+        reserva.setIdaEvolta(idaEvolta);
+        reserva.setPoltrona(poltrona);
         reserva.setOrigem(origem);
         reserva.setDestino(destino);
-        reserva.setDataVoo(data);
+
+        if (idaEvolta) {
+            System.out.println("Preencha os dados da volta:");
+            System.out.print("Data da volta (dd/mm/aaaa): ");
+            String dataVolta = sc.nextLine();
+            System.out.print("Hora da volta (hh:mm): ");
+            String horaVolta = sc.nextLine();
+            System.out.print("Preço da passagem de volta: R$ ");
+            float precoVolta = sc.nextFloat();
+            sc.nextLine();
+            System.out.print("Classe da volta: ");
+            String classeVolta = sc.nextLine();
+            System.out.print("Número da poltrona da volta: ");
+            int poltronaVolta = sc.nextInt();
+            sc.nextLine();
+
+            Reserva reservaVolta = new Reserva();
+            reservaVolta.setNumReserva(numReserva + 1);
+            reservaVolta.setDataVoo(dataVolta);
+            reservaVolta.setHoraVoo(horaVolta);
+            reservaVolta.setPreco(precoVolta);
+            reservaVolta.setClasseVoo(classeVolta);
+            reservaVolta.setIdaEvolta(false);
+            reservaVolta.setPoltrona(poltronaVolta);
+            reservaVolta.setOrigem(destino);
+            reservaVolta.setDestino(origem);
+
+            reserva.setVolta(reservaVolta);
+        }
 
         sistema.reservarIda(cliente, reserva);
 
